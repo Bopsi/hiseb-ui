a9ea38b5a57fc571bec2ef112badd14223aa245d
balakagit <balakachatterjee90@gmail.com>
2023-09-30 23:48:30 +0530

    component set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
+import { AddComponent } from './expenses/add/add.component';
+import { EditComponent } from './expenses/edit/edit.component';
+import { ListComponent } from './expenses/list/list.component';
+import { AllComponent } from './charts/all/all.component';
+import { PersonComponent } from './charts/person/person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    EditComponent,
+    ListComponent,
+    AllComponent,
+    PersonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
